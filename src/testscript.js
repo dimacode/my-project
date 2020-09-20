a533306c86824df7fcf578503c33827597614921
DevBrother2019 <dmitryb@devbrother.com>
2020-09-20 22:18:09 +0300

    testscript

--- src/testscript.js
+++ src/testscript.js
@@ -6,14 +6,14 @@ const crypto = require("crypto");
 const baseUrl = "https://api.binance.com/";
 
 // ORDERS and ACCOUNT
-const getAccountData = "api/v3/account";
-const getLatestPrice = "api/v3/ticker/price";
-const sendOrder = "api/v3/order/test";
+const accountData = "api/v3/account";
+const latestPrice = "api/v3/ticker/price";
+const order = "api/v3/order/test";
 
 let timeOffset = 0;
 const recvWindow = 5000;
 
-function buildQuery(data) {
+const buildQuery = data => {
   return Object.keys(data)
     .reduce(function (a, k) {
       a.push(k + "=" + encodeURIComponent(data[k]));
@@ -22,7 +22,8 @@ function buildQuery(data) {
     .join("&");
 }
 
-function getAccount(apiKey, apiSecret, data = {}) {
+// GET ACCOUNT
+const getAccountData = (apiKey, apiSecret, data = {}) => {
   if (!apiKey || !apiSecret) {
     throw new Error(
       "You need to pass an API key and secret to make authenticated calls."
@@ -32,17 +33,53 @@ function getAccount(apiKey, apiSecret, data = {}) {
   data.timestamp = new Date().getTime() + timeOffset;
   if (typeof data.recvWindow === "undefined") data.recvWindow = recvWindow;
 
-  data.symbol = 'TRXBTC';
-  data.side = 'sell';
-  data.type = 'MARKET';
-  data.quantity = 500;
+  const query = buildQuery(data);
+  const signature = crypto
+    .createHmac("sha256", apiSecret)
+    .update(query)
+    .digest("hex");
+  const url = baseUrl + accountData + "?" + query + "&signature=" + signature;
+  return axios
+    .get(url, {
+      headers: {
+        "X-MBX-APIKEY": apiKey,
+      },
+    })
+    .then(x=>x.data)
+    .catch((err) => console.log("error:", err));
+}
+
+// getAccountData(access_key, secret_key, {}).then(account=>{
+//     console.log("account", account)
+// })
+
+// GET LATEST PRICE
+const getLatestPrice = (pair = 'TRXBTC') => axios.get(`${baseUrl}${latestPrice}?symbol=${pair}`)
+  .then(res => res.data)
+
+// SEND ORDER
+const sendOrder = (apiKey, apiSecret, data) => {
+
+  if (!apiKey || !apiSecret) {
+    throw new Error(
+      "You need to pass an API key and secret to make authenticated calls."
+    );
+  }
+  
+  data.timestamp = new Date().getTime() + timeOffset;
+  if (typeof data.recvWindow === "undefined") data.recvWindow = recvWindow;
+
+  // data.symbol = 'TRXBTC';
+  // data.side = 'sell';
+  // data.type = 'MARKET';
+  // data.quantity = 500;
 
   const query = buildQuery(data);
   const signature = crypto
     .createHmac("sha256", apiSecret)
     .update(query)
     .digest("hex");
-  const url = baseUrl + getAccountData + "?" + query + "&signature=" + signature;
+  const url = baseUrl + order + "?" + query + "&signature=" + signature;
   return axios
     .post(url, '', {
       headers: {
@@ -53,6 +90,12 @@ function getAccount(apiKey, apiSecret, data = {}) {
     .catch((err) => console.log("error:", err));
 }
 
-getAccount(access_key, secret_key, {}).then(account=>{
-    console.log("account", account)
-})
+// sendOrder(access_key, secret_key, {
+//   symbol: 'TRXBTC',
+//   side: 'sell',
+//   type: 'MARKET',
+//   quantity: 500,
+// }).then(account=>{
+//   console.log("account", account)
+// });
+
